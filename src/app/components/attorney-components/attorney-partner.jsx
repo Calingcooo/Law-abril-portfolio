39ade8d7e59ee02cf1d60d451e65efc34c9ff0ab
DelrosarioRovic <rovicdelrosario01@gmail.com>
2024-07-12 10:19:33 +0800

    added trusted companies

--- src/app/components/attorney-components/attorney-partner.jsx
+++ src/app/components/attorney-components/attorney-partner.jsx
@@ -4,13 +4,18 @@ import Image from 'next/image';
 import luelco from "../../../../public/luelco.jpeg";
 import uppfi from "../../../../public/uppfi.jpg";
 import diolazo from "../../../../public/Diolazo Press.jpg";
+import amc from "../../../../public/amc.jpg";
+import bombo from "../../../../public/bomboradyo.jpg";
+import lawinLogo from "../../../../public/LawinLogo.png";
+import nkr from "../../../../public/nkr.png";
+import ojo from "../../../../public/ojo-majica.jpg";
 
-const partnerCompany = [uppfi, luelco, diolazo];
+const partnerCompany = [uppfi, luelco, diolazo, amc, bombo, lawinLogo, nkr, ojo];
 const AttorneyPartner = () => {
   return (
     <div className='max-w-[75rem] w-full max-lg:px-10 max-lg:flex-col m-auto flex flex-col gap-10  px-14 py-20 '>
         <h1 className='text-gray-800 font-bold text-2xl hightlightText'>Trusted by these Companies</h1>
-        <div className='flex justify-between items-center w-full'>
+        <div className='flex justify-between items-center max-sm:justify-center w-full flex-wrap gap-10'>
               {partnerCompany.map((partner, index) => (
                 <div 
                   key={index} 
@@ -18,7 +23,7 @@ const AttorneyPartner = () => {
                   data-aos="zoom-in"
                   data-aos-delay={(index + 1) * 100}
                 >
-                  <Image className='max-sm:hidden' alt='partnership' 
+                  <Image className='' alt='partnership' 
                     key={index} 
                     src={partner}
                   /> 
