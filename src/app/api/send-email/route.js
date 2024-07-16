2926c028c05163bb3f375c70d213524adf153548
Calingcooo <calingco.je@gmail.com>
2024-07-16 16:20:14 +0800

    Add 'email' in the content body of the inquiry message

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -26,30 +26,31 @@ export async function POST(request) {
     disclaimer,
   } = Object.fromEntries(formData);
 
-  // const transporter = nodemailer.createTransport({
-  //   host: "smtp.hostinger.com",
-  //   port: 465, // Port (use 587 for TLS, 465 for SSL)
-  //   secure: true, // true for 465, false for other ports
-  //   auth: {
-  //     user: `${process.env.NEXT_APP_INQUIRY_EMAIL}`,
-  //     pass: `${process.env.NEXT_APP_INQUIRY_EMAIL_PASS}`,
-  //   },
-  // });
+  const transporter = nodemailer.createTransport({
+    host: "smtp.hostinger.com",
+    port: 465, // Port (use 587 for TLS, 465 for SSL)
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: `${process.env.NEXT_APP_INQUIRY_EMAIL}`,
+      pass: `${process.env.NEXT_APP_INQUIRY_EMAIL_PASS}`,
+    },
+  });
 
-  // const emailContent = `
-  // <p>Name: ${first} ${last}</p>
-  // <p>Contact Number: ${cnumber}</p>
-  // <p>Address: ${address_1} ${address_2}, ${region}, ${city}, ${barangay}</p>
-  // <p>Communication Preference: ${comm_pref}</p>
-  // <p>Inquiry: ${content}</p>
-  // `;
+  const emailContent = `
+  <p>Name: ${first} ${last}</p>
+  <p>Email Address: ${email}</p>
+  <p>Contact Number: ${cnumber}</p>
+  <p>Address: ${address_1} ${address_2}, ${region}, ${city}, ${barangay}</p>
+  <p>Communication Preference: ${comm_pref}</p>
+  <p>Inquiry: ${content}</p>
+  `;
 
-  // const mailOptions = {
-  //   from: `${process.env.NEXT_APP_INQUIRY_EMAIL}`,
-  //   to: `${process.env.NEXT_APP_INQUIRY_EMAIL}`,
-  //   subject: "Online Concern/Inquiry",
-  //   html: emailContent,
-  // };
+  const mailOptions = {
+    from: `${process.env.NEXT_APP_INQUIRY_EMAIL}`,
+    to: `${process.env.NEXT_APP_INQUIRY_EMAIL}`,
+    subject: "Online Concern/Inquiry",
+    html: emailContent,
+  };
 
   if (disclaimer !== "on") {
     return Response.json(
@@ -58,17 +59,17 @@ export async function POST(request) {
     );
   }
 
-  // try {
-  //   await transporter.sendMail(mailOptions);
-  //   return Response.json(
-  //     { success: true, message: "Inquiry sent successfully!" },
-  //     { status: 200 }
-  //   );
-  // } catch (error) {
-  //   console.error(error);
-  //   return Response.json(
-  //     { success: false, message: `Failed to send email. ${error}` },
-  //     { status: 500 }
-  //   );
-  // }
+  try {
+    await transporter.sendMail(mailOptions);
+    return Response.json(
+      { success: true, message: "Inquiry sent successfully!" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { success: false, message: `Failed to send email. ${error}` },
+      { status: 500 }
+    );
+  }
 }
