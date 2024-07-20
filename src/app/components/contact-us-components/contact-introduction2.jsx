1719d4a257c52a579fa384d022a1ebef3a484d5e
DelrosarioRovic <rovicdelrosario01@gmail.com>
2024-07-20 11:30:14 +0800

    develop-staging

--- src/app/components/contact-us-components/contact-introduction2.jsx
+++ src/app/components/contact-us-components/contact-introduction2.jsx
@@ -4,8 +4,8 @@ const ContactIntroduction2 = () => {
   return (
     <div className='eventBg'>
         <div className='flex flex-col max-w-[75rem] w-full max-lg:px-10 m-auto gap-10 justify-center items-center max-md:items-start py-20'>
-            <h1 className='text-5xl max-sm:text-4xl hightlightText font-bold text-white'>Solve your Clients Legal Problems</h1>
-            <p className='text-lg text-white'>We are dedicated to providing expert legal solutions</p>
+            <h1 className='text-5xl max-sm:text-4xl hightlightText font-bold text-white'>Your Path to Legal Resolution Begins Here</h1>
+            <p className='text-lg text-white'>Share your problem, and we'll craft the solution.</p>
         </div>
     </div>
   )
--- src/app/components/practice-area-components/practice-introduction.jsx
+++ src/app/components/practice-area-components/practice-introduction.jsx
@@ -9,7 +9,7 @@ const PracticeIntroduction = () => {
             <div className='w-[45%] max-lg:w-full flex flex-col gap-5 py-10'>
                 <h1 className='text-4xl font-bold hightlightText text-gray-900'>Practice Areas</h1>
                 <h2 className='text-xl font-bold text-gray-800 hightlightText'>Comprehensive Legal Services Tailored to Your Needs</h2>
-                <p className='text-gray-800 text-sm'>At Abril Law Office, we pride ourselves on offering a wide array of legal services to address the diverse needs of our clients. Our experienced attorneys specialize in various practice areas, ensuring that you receive expert advice and effective solutions tailored to your specific situation. Whether you are an individual seeking legal assistance or a business in need of comprehensive legal support, we are here to help you navigate the complexities of the legal landscape.</p>
+                <p className='text-gray-800 text-sm'>Abril Law Office, takes pride in offering a wide array of legal services to address the diverse needs of our clients. Our experienced team specializes in various practice areas, ensuring that you receive expert advice and effective solutions tailored to your specific situation. Whether you are an individual seeking legal assistance or a business in need of comprehensive legal support, we are here to help you navigate the complexities of the legal landscape.</p>
             </div>
             <div className='w-[55%] flex max-md:w-full max-lg:w-[80%] justify-center'>
                 <Image src={abrilLawLogo} className='w-[60%]'/>
